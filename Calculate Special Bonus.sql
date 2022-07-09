21dfe68ec14219839b017bfd096e81ebb1ac426d
codemugger <66456913+codemugger@users.noreply.github.com>
2022-07-09 17:44:53 +0800

    Update Calculate Special Bonus.sql

--- Calculate Special Bonus.sql
+++ Calculate Special Bonus.sql
@@ -4,3 +4,20 @@ CASE
     ELSE 0 
 END AS bonus 
 FROM employees
+
+'''
+Alternative Solution: 
+# Write your MySQL query statement below
+SELECT employee_id , 
+IF (employee_id % 2 AND name NOT LIKE "M%", salary,0) AS bonus 
+FROM employees 
+'''
+
+'''
+2nd Alternative Solution 
+SELECT employee_id, 
+(salary * (name NOT LIKE "M%") * (employee_id % 2 <> 0) ) AS bonus 
+FROM employees 
+ORDER BY employee_id 
+
+'''
