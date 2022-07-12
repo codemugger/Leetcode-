ec6977b0fd576690281c0321278ceb87c8c2b428
codemugger <66456913+codemugger@users.noreply.github.com>
2022-07-12 14:58:36 +0800

    Create Employees with missing information.sql

---
+++ Employees with missing information.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT employee_id FROM Employees WHERE  employee_id NOT IN (SELECT employee_id FROM Salaries WHERE salary IS NOT NULL)
+UNION 
+SELECT employee_id FROM Salaries WHERE employee_id NOT IN (SELECT employee_id FROM Employees WHERE name IS NOT NULL)
+ORDER BY 1 ASC 
